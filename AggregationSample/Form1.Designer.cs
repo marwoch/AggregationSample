59abcee305c79ab989f1834a9c7dc5cf37c51690
marwoch <marwoch@users.noreply.github.com>
2017-08-08 15:13:52 -0700

    8aug17

--- AggregationSample/Form1.Designer.cs
+++ AggregationSample/Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lstCourses = new System.Windows.Forms.ListBox();
+            this.btnViewInstructor = new System.Windows.Forms.Button();
+            this.btnViewRoster = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstCourses
+            // 
+            this.lstCourses.FormattingEnabled = true;
+            this.lstCourses.Location = new System.Drawing.Point(13, 13);
+            this.lstCourses.Name = "lstCourses";
+            this.lstCourses.Size = new System.Drawing.Size(120, 95);
+            this.lstCourses.TabIndex = 0;
+            // 
+            // btnViewInstructor
+            // 
+            this.btnViewInstructor.Location = new System.Drawing.Point(148, 13);
+            this.btnViewInstructor.Name = "btnViewInstructor";
+            this.btnViewInstructor.Size = new System.Drawing.Size(115, 42);
+            this.btnViewInstructor.TabIndex = 1;
+            this.btnViewInstructor.Text = "Instructor";
+            this.btnViewInstructor.UseVisualStyleBackColor = true;
+            this.btnViewInstructor.Click += new System.EventHandler(this.btnViewInstructor_Click);
+            // 
+            // btnViewRoster
+            // 
+            this.btnViewRoster.Location = new System.Drawing.Point(148, 66);
+            this.btnViewRoster.Name = "btnViewRoster";
+            this.btnViewRoster.Size = new System.Drawing.Size(115, 42);
+            this.btnViewRoster.TabIndex = 2;
+            this.btnViewRoster.Text = "Roster";
+            this.btnViewRoster.UseVisualStyleBackColor = true;
+            this.btnViewRoster.Click += new System.EventHandler(this.btnViewRoster_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 145);
+            this.Controls.Add(this.btnViewRoster);
+            this.Controls.Add(this.btnViewInstructor);
+            this.Controls.Add(this.lstCourses);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstCourses;
+        private System.Windows.Forms.Button btnViewInstructor;
+        private System.Windows.Forms.Button btnViewRoster;
     }
 }
 
--- AggregationSample/Form1.cs
+++ AggregationSample/Form1.cs
@@ -16,5 +16,62 @@ namespace AggregationSample
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Course c1 = new Course()
+            {
+                Title = "Advanced .NET",
+                Number = "CPW 212",
+                NumCredits = 5
+            };
+
+            Course c2 = new Course()
+            {
+                Title = ".NET Web",
+                Number = "CPW 213",
+                NumCredits = 5
+            };
+
+            var courses = new List<Course>()
+            {
+                c1,
+                c2
+            };
+            //lstCourses.Items.Add(c1);
+            //lstCourses.Items.Add(c2);
+
+            lstCourses.DataSource = courses;
+            lstCourses.DisplayMember = nameof(Course.Title);
+        }
+
+        private void btnViewInstructor_Click(object sender, EventArgs e)
+        {
+            //Course c = GetSelectedCourse();
+            //Instructor courseInstructor = c.Instructor;
+            //MessageBox.Show(courseInstructor.Name.ToUpper());
+
+            Course c = GetSelectedCourse();
+            MessageBox.Show(c.Instructor.Name.ToUpper());
+
+            //MessageBox.Show(GetSelectedCourse().Instructor.Name.ToUpper());
+        }
+
+        private void btnViewRoster_Click(object sender, EventArgs e)
+        {
+            Course c = GetSelectedCourse();
+            var display = new StringBuilder();
+            foreach (Student s in c.Roster)
+            {
+                display.AppendLine($"{s.FirstName} {s.LastName}");
+            }
+            MessageBox.Show(display.ToString());
+        }
+
+        private Course GetSelectedCourse()
+        {
+            return (Course)lstCourses.SelectedItem;
+        }
     }
+
 }
